df5aca1288e28754db853efc5acf7546f89bacac
FauzanAQD <muhammadfauzan11keren@gmail.com>
2024-12-04 22:34:59 +0700

    progress2fauzan

---
+++ Exersice.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Exersice{
+    int durasi;
+    String nama;
+
+    public Exersice(int durasi, String nama){
+        this.durasi = durasi;
+        this.nama = nama;
+    }
+}
